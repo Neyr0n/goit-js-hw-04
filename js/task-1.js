99455a6a00859539ffbed2e3648fda0b74301680
Vladislav <neyron232@gmail.com>
2024-04-14 17:31:57 +0200

    solved all the tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,14 @@
-function makeTransaction(quantity, pricePerDroid) {
-  return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
+function isEnoughCapacity(products, containerSize) {
+  let totalProducts = 0;
+  for (const product of Object.values(products)) {
+    totalProducts += product;
+  }
+  if (totalProducts <= containerSize) {
+    return true;
+  }
+  return false;
 }
-
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
